0d55f9512554b501af78d8784fa59225bfe942e5
Kerolayne <karolgrassi.d@hotmail.com>
2026-06-20 20:50:27 -0300

    feat: integrando leitura de contatos e envio de mensagens

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from supabase_service import get_contacts
+from zapi_service import send_message
 
 contacts = get_contacts()
 
-print(contacts)
+for contact in contacts:
+    name = contact["nome"]
+    phone = contact["telefone"]
+
+    message = f"Olá, {name} tudo bem com você?"
+
+    response = send_message(phone, message)
+
+    print(f"Mensagem enviada para {name}")
