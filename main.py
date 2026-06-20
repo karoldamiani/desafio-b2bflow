2570f6dc1c95b3d60a5e06384d267e51839f37df
Kerolayne <karolgrassi.d@hotmail.com>
2026-06-20 20:57:50 -0300

    feat: adicionando log e tratamento de erros

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from supabase_service import get_contacts
 from zapi_service import send_message
 
-contacts = get_contacts()
+try:
+    contacts = get_contacts()
 
-for contact in contacts:
-    name = contact["nome"]
-    phone = contact["telefone"]
+    for contact in contacts:
+        name = contact["nome"]
+        phone = contact["telefone"]
 
-    message = f"Olá, {name} tudo bem com você?"
+        message = (
+             f"Olá, {name} tudo bem com você?"
+        )
 
-    response = send_message(phone, message)
+        response = send_message(
+            phone, 
+            message
+        )
 
-    print(f"Mensagem enviada para {name}")
+        print(f"{name} - Status: {response.status_code}")
+
+except Exception as error:
+    print(f"Error: {error}")
